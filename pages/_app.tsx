5be2844fe728b99c6cbcac32495827e1e4ae4bc8
Kyungjin Jung <kyoooungjin@gmail.com>
2023-09-22 10:50:28 +0900

    search-page layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,12 @@ import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
+import "../styles/main.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <Component {...pageProps} />
       <Footer />
     </div>
---
+++ pages/search/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+import * as SC from "./styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import SearchBar from "../../components/input/SearchBar";
+
+const Search: React.FC = () => {
+  return (
+    <SC.Container>
+      <SC.SearchTop>
+        <SearchBar />
+      </SC.SearchTop>
+      <SC.Content>
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+        <Image src="/images/coffee.jpg" alt="게시글" width={135} height={135} />
+      </SC.Content>
+      <SC.Loading>
+        <FontAwesomeIcon icon={faSpinner} fontSize={"25px"} />
+      </SC.Loading>
+    </SC.Container>
+  );
+};
+
+export default Search;
