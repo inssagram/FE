57702d1692378c1528f728668c2353544e2f9125
Daily-manjoo <yuslz35@gmail.com>
2023-10-05 22:15:08 +0900

    게시물 올리기 테스트 진행중

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquarePlus, faHeart } from "@fortawesome/free-regular-svg-icons";
 import * as SC from "../components/styledHeader";
 import { useRef } from "react";
+import axios from "axios";
 
 const Header: React.FC = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -11,10 +12,19 @@ const Header: React.FC = () => {
     fileInputRef.current?.click();
   };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const file = e.target.files[0];
-      console.log(file);
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 파일 선택하더라도 정해진 이미지 주소만 사용
+    const fixedImageSrc = "https://www.animals.or.kr/api/files/images/51386-cb98b35c-a5ac-484c-b365-f73e8263bd05.png";
+
+    try {
+      const response = await axios.post("http://localhost:4000/posts", {
+        image: fixedImageSrc,
+      });
+      if (response.status === 201) {
+        alert("Image uploaded!");
+      }
+    } catch (error) {
+      alert("Failed to upload the image");
     }
   };
 
--- pages/my/login/index.tsx
+++ pages/my/login/index.tsx
@@ -1,18 +1,61 @@
 import * as SC from "./styled";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 const Login: React.FC = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/users", {
+        params: {
+          email: id,
+          password: password,
+        },
+      });
+
+      // 사용자 검증하기
+      if (response.data && response.data.length > 0) {
+        const user = response.data[0];
+
+        const tokenResponse = await axios.get("http://localhost:4000/tokens", {
+          params: {
+            userId: user.id,
+          },
+        });
+
+        if (tokenResponse.data && tokenResponse.data.length > 0) {
+          const token = tokenResponse.data[0].token;
+          // 토큰을 LocalStorage에 저장하기
+          localStorage.setItem("token", token);
+
+          console.log("토큰: ", token);
+          router.push("/main");
+        } else {
+          alert("토큰을 받아오는데 실패했습니다.");
+        }
+      } else {
+        alert("로그인 정보가 올바르지 않습니다.");
+      }
+    } catch (error) {
+      alert("로그인 중 오류가 발생했습니다.");
+    }
+  };
+
   return (
     <SC.Container>
       <SC.Notification>
         <Image src="/images/logo.png" alt="Logo" width={60} height={60} />
         <SC.Title>Inssagram</SC.Title>
         <SC.LoginCont>
-          <SC.Id alt="이메일입력" placeholder="이메일을 입력하세요" />
-          <SC.Password alt="비밀번호입력" placeholder="비밀번호를 입력하세요" />
+          <SC.Id alt="이메일입력" placeholder="이메일을 입력하세요" onChange={(e) => setId(e.target.value)} />
+          <SC.Password alt="비밀번호입력" placeholder="비밀번호를 입력하세요" onChange={(e) => setPassword(e.target.value)} />
           <SC.FindPassword>비밀번호를 잊으셨나요?</SC.FindPassword>
-          <SC.Login>로그인</SC.Login>
+          <SC.Login onClick={handleLogin}>로그인</SC.Login>
         </SC.LoginCont>
         <SC.NotaMember>
           계정이 없으신가요?<SC.SignIn>가입하기</SC.SignIn>
