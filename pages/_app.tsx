e2408c52cd3645ffec16cf666d50eda47a1d910c
Kyungjin Jung <kyoooungjin@gmail.com>
2023-10-19 18:34:30 +0900

    delete: scss import statement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import "../styles/main.css";
-import "../components/styled/spinner.scss";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
