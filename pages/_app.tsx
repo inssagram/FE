736a49571b8fee5e9f0edf3d5e3168cbd1a6cd10
Daily-manjoo <122348173+Daily-manjoo@users.noreply.github.com>
2023-10-21 16:29:47 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import "../styles/main.css";
-import '../components/styled/spinner.scss'
 import { Provider } from "react-redux";
 import { store } from "@/src/redux/Posts/store";
 const App = ({ Component, pageProps }: AppProps)=>{
