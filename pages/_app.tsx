4c883bf4397a52bb3ccfe1221fb603aa2ea1e25d
Daily-manjoo <yuslz35@gmail.com>
2023-10-25 15:38:29 +0900

    profile 소개글 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import "../styles/main.css";
-import '../components/styled/spinner.scss'
 import { Provider } from "react-redux";
-import { store } from "@/src/redux/Posts/store";
+import { persistor, store } from "@/src/redux/Posts/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 const App = ({ Component, pageProps }: AppProps)=>{
   return (
     <>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <Component {...pageProps} />
+      </PersistGate>
       </Provider>
     </>
   );
