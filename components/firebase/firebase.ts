af105ecd7ef1b19754dafc588655313c77cd827c
염정호(Josh) <105287510+joshyeom@users.noreply.github.com>
2024-07-12 19:08:45 +0900

    Update firebase.ts

--- components/firebase/firebase.ts
+++ components/firebase/firebase.ts
@@ -2,15 +2,7 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC-b-qg4zxIxvX-pOGIbgubojBcLjNqSzQ",
-  authDomain: "inssagram-79518.firebaseapp.com",
-  projectId: "inssagram-79518",
-  storageBucket: "inssagram-79518.appspot.com",
-  messagingSenderId: "903531383478",
-  appId: "1:903531383478:web:72eeccfd6899f428cb7417",
-  measurementId: "G-BV7JHREQKR"
-};
+
 
 
 const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
